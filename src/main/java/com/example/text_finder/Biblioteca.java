2189208b4cc8998b7b6b6bb0e74da63c7957033d
MauroCR52 <mauluna52@gmail.com>
2022-10-17 21:52:14 -0600

    Leer documentos de texto y obtener el nombre, fecha y tamaño

--- src/main/java/com/example/text_finder/Biblioteca.java
+++ src/main/java/com/example/text_finder/Biblioteca.java
@@ -2,20 +2,27 @@ package com.example.text_finder;
 
 public class Biblioteca {
     public Nodo_Biblioteca head;
+    public Nodo_Biblioteca tail;
     private int size;
 
-    public Biblioteca(){
+    public Biblioteca() {
         this.head = null;
         this.size = 0;
     }
-    public int getSize(){
+
+    public int getSize() {
         return this.size;
     }
-    public void InsertarDocumento(Documento documento){
+
+    public void InsertarDocumento(Documento documento) {
         Nodo_Biblioteca nodo_biblioteca = new Nodo_Biblioteca(documento, null);
         nodo_biblioteca.next = this.head;
         this.head = nodo_biblioteca;
         this.size++;
+    }
+
+    public void BubbleSort(){
 
     }
+    public static Biblioteca biblioteca = new Biblioteca();
 }
